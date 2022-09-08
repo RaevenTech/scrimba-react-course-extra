e8bea0c11376f69a34427529fc54553b1ebdc8c6
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-08 17:00:15 +0100

    tailwind added to the project

--- scrimba-course-basic/src/App.js
+++ scrimba-course-basic/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MarkUp from "./components/projectPartOne/MarkUp";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>
+                <MarkUp />
+            </h1>
+        </div>
+    );
 }
 
 export default App;
